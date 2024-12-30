eb4d9842e8481c6d0028e00cce2ea3cb3c8dc2c4
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-12-30 18:24:05 +0530

    update auth layout to include Claryfi logo and link

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,11 +10,26 @@ const AuthLayout = ({
 }>) => {
   return (
     <div className="grid place-items-center h-screen mx-auto">
-      <div className="h-full flex flex-col p-6 lg:p-8 2xl:px-24">
-        <div className="flex items-center gap-2 text-lg font-title font-semibold">
-          <FrameIcon className="w-5 h-5 text-primary" />
-          Acme Inc
-        </div>
+      <div className="h-full flex flex-col items-center p-6 lg:p-8 2xl:px-24">
+        <Link href="/">
+          <span className="flex items-center space-x-1.5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="none"
+            >
+              <rect width="30" height="30" fill="#111827" rx="4" />
+              <path
+                fill="#fff"
+                d="M25 15a10 10 0 0 1-20 0h4a6 6 0 0 0 12 0h4Z"
+              />
+              <path fill="#fff" d="M17 17h-4V7h4z" />
+              <path fill="#fff" d="M15 11V7h10v4z" />
+            </svg>
+            <span className="font-bold text-lg">Claryfi</span>
+          </span>
+        </Link>
         <main className="h-full flex items-center justify-between">
           {children}
         </main>
